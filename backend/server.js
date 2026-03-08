5eff1f3110282a41c27382cc4ed5306e3d980316
JohnCarloReyes22 <22-03171@g.batstate-u.edu.ph>
2026-03-08 15:04:28 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.set('trust proxy', 1);
 
 // MySQL connection
 const db = mysql.createConnection({
